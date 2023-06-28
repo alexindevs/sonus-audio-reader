af3876ca714fb6a55bfc78297e7b63f78e3820d4
alexindevs <alexindevs@gmail.com>
2023-06-28 20:25:15 +0100

    fix stuff

--- popup.js
+++ popup.js
@@ -12,28 +12,29 @@
     var statusText = document.getElementById("status");
 
     function startScrape() {
-      // Update UI to indicate scraping is in progress
       startButton.style.backgroundColor = "#040354"
       statusText.textContent = 'Scraping in progress...';
     
-      // Popup Script
-      chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-        chrome.tabs.sendMessage(tabs[0].id, { action: 'startCapture' }, response => {
-          // Handle the response from content script
-          const scrapedText = response.scrapedText;
-    
-          if (scrapedText) {
-            // Send the scraped text to the background script
-            chrome.runtime.sendMessage({ action: 'convertToAudio', text: scrapedText });
-    
-            startButton.disabled = true;
-            statusText.textContent = 'Scraping complete';
-          } else {
-            // Update UI to indicate no text was scraped
-            statusText.textContent = 'No text found on the page';
-          }
-        });
-      });
-    }
+      
+      var scrapedText = null; 
+chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+  chrome.tabs.sendMessage(tabs[0].id, { action: 'startCapture' }, response => {
+    scrapedText = response && response.scrapedText ? response.scrapedText : null;
 
+    if (scrapedText) {
+      // Send the scraped text to the background script
+      chrome.runtime.sendMessage({ action: 'convertToAudio', text: scrapedText });
+
+      // Update UI to indicate scraping is done
+      startButton.disabled = false;
+      statusText.textContent = 'Scraping complete';
+    } else {
+      // Update UI to indicate no text was scraped
+      statusText.textContent = 'No text found on the page';
+    }
+  });
+});
+
+      "I'm trying to build an extension, and it's purpose requires me to scrape the text of the current tab, and store in a variable. Now, I'm trying to check if that worked the way it should, by console logging the value of the variable. But it's not working in the extensions devtools, AND in the website's devtools. Here is the code snippet from the frontend script (controlling the UI as the process goes on). "
+}
     startButton.addEventListener("click", startScrape);
