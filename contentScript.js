ca8de26b445003ffd4542cae4078c55882508839
alexindevs <alexindevs@gmail.com>
2023-07-01 12:51:14 +0100

    Content script verification

--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,7 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'startCapture') {
-    const textContent = Array.from(document.querySelectorAll('*')).map(element => element.textContent).join(' ');
+    const textContent = Array.from(document.querySelectorAll('h1, h2, h3, h4, h5, h6, p, span, div:not(script), a:not(nav):not(.Navbar):not(#navbar):not(Header):not(.header):not(#header)')).map(element => element.textContent)
+    .join(' ');
     chrome.runtime.sendMessage({ action: 'scrapedText', content: textContent }, response => {
       sendResponse({ scrapedText: textContent });
     });
