0966571709d9e127d70f72a1598252b296522092
Steph4n <108176326+Lkjhhgffdssaaaa@users.noreply.github.com>
2023-07-10 23:11:05 +0100

    hiii

--- popup.js
+++ popup.js
@@ -1,49 +1,59 @@
 // document.addEventListener('DOMContentLoaded', function() {
-  
-    var preloader = document.querySelector('#opening');
-    var startButton = document.querySelector("#start")
- 
-    function addGoneClass() {
-      preloader.classList.add('gone');
-      startButton.style.display = "block";
-    }
-  
-    setTimeout(addGoneClass, 10000);
-    var statusText = document.getElementById("status");
 
-    var scrapedText;
-    function startScrape() {
-       startButton.style.backgroundColor = "#040354";
-      statusText.textContent = 'Scraping in progress...';
-    
-      
+var preloader = document.querySelector('#opening');
+var startButton = document.querySelector("#start")
+
+function addGoneClass() {
+  preloader.classList.add('gone');
+  startButton.style.display = "block";
+}
+
+setTimeout(addGoneClass, 10000);
+var statusText = document.getElementById("status");
+
+var scrapedText;
+function startScrape() {
+  startButton.style.backgroundColor = "#040354";
+  statusText.textContent = 'Scraping in progress...';
+
+
   chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-  chrome.tabs.sendMessage(tabs[0].id, { action: 'startCapture' }, response => {
-    scrapedText = response && response.scrapedText ? response.scrapedText : null;
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'startCapture' }, response => {
+      scrapedText = response && response.scrapedText ? response.scrapedText : null;
 
-    if (scrapedText) {
-      // Send the scraped text to the background script
-      chrome.runtime.sendMessage({ action: 'convertToAudio', text: scrapedText });
+      if (scrapedText) {
+        // Send the scraped text to the background script
+        chrome.runtime.sendMessage({ action: 'convertToAudio', text: scrapedText });
 
-      // Update UI to indicate scraping is done
-      startButton.disabled = false;
-      statusText.textContent = 'Scraping complete';
-    } else {
-      // Update UI to indicate no text was scraped
-      statusText.textContent = 'No text found on the page';
-    }
+        // Update UI to indicate scraping is done
+        startButton.disabled = false;
+        statusText.textContent = 'Scraping complete';
+      } else {
+        // Update UI to indicate no text was scraped
+        statusText.textContent = 'No text found on the page';
+      }
+    });
   });
-});
 
 }
-    startButton.addEventListener("click", startScrape);
+startButton.addEventListener("click", startScrape);
 
-    startButton.addEventListener("click", displayFunc);
 
-    var cont = document.getElementById('container');
-    cont.style.display = "none";
 
-    function displayFunc(){
-        startButton.style.display = "none";
-          cont.style.display = "block";
-      };
+var cont = document.getElementById('container');
+cont.style.display = "none";
+
+function displayFunc() {
+  startButton.style.display = "none";
+  cont.style.display = "block";
+};
+
+startButton.addEventListener("click", displayFunc);
+
+// function hideFunc() {
+//   startButton.style.display = "block";
+//   cont.style.display = "none";
+// };
+// setTimeout(hideFunc, 2000);
+
+// document.querySelector("body").addEventListener("click", hideFunc);
